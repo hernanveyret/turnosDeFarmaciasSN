ec8a53329d642140371c9b096f0f49599f6168c3
hernanveyret <hernanveyret@hotmail.com>
2024-09-09 10:39:52 -0300

    Correccion de errores

--- src/Componentes/Peticiones.js
+++ src/Componentes/Peticiones.js
@@ -7,13 +7,22 @@ const Peticiones = ({hora, day, month, year, setLoader }) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
+  const convertToTime = (timeStr) => {
+    const [hours, minutes, seconds] = timeStr.split(':');
+    return new Date(1970, 0, 1, hours, minutes, seconds).getTime();
+  };
+
+  const horaActual = convertToTime(hora);
+  const inicio = convertToTime('00:00:00');
+  const fin = convertToTime('08:30:00');
+
   let url = `https://server-farmacias.vercel.app/api/farmacias/${year}/${month + 1}/${day}`
   
 
   useEffect(() => {
    
-    if(hora > '00:00:00' && hora < '8:30:00'){
-      
+    if(horaActual >= inicio && horaActual <= fin){
+      console.log('muestra letra del dia anteior')
       setLoader(true);
       axios.get(`https://server-farmacias.vercel.app/api/farmacias/${year}/${month + 1}/${day - 1}`)
         .then(response => {
@@ -29,6 +38,7 @@ const Peticiones = ({hora, day, month, year, setLoader }) => {
         });
 
     }else{
+      console.log('muestra letra del dia actual')
       setLoader(true);
       axios.get(url)
       .then(response => {
