6a9b6c483fceaf9468823f59396fca74514fdb4f
hernanveyret <hernanveyret@hotmail.com>
2024-09-12 10:33:56 -0300

    Actualizacion

--- src/Componentes/Home.js
+++ src/Componentes/Home.js
@@ -154,6 +154,7 @@ const Home = () => {
     setMonthString(meces.find(m => m.id === month).mes)
   },[month,year, meces])
 
+  // Selecciona el dia en el almanaque por mes.
   // el metodo trim() elimina los espacios vacios de lo que traiga el target
   const handleDay = (e) => {
     let textContent = e.currentTarget.textContent.trim();
@@ -217,7 +218,9 @@ const Home = () => {
       { mapsOn && <Maps /> }
          { loader && <Loader />}
       <header ref={headerRef}>
-        <h1>Farmacias de turno SN</h1>
+        <a href="https://turnos-de-farmacias-sn.vercel.app/">
+          <h1>Farmacias de turno SN</h1>
+        </a>
         <button className="btn-menu" onClick={settingOptions} title="Abrir menu">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z"/></svg>
         </button>
@@ -258,7 +261,6 @@ const Home = () => {
           }  
         </article>
         <article className="farmacias">
-        <h2>Datos de Farmacias</h2>
             <Peticiones
               hora={hora}
               day={day}
@@ -269,13 +271,10 @@ const Home = () => {
         </article>
       </main>
       <footer ref={fotterRef}>
-        <p>Hernán Luis Veyret-2024</p>
+        <p>Diseñado por Hernán Luis Veyret - 2024 - Version 1.1 - hernanveyret@hotmail.com</p>
       </footer>
     </div>
   )
 }
 
 export default Home;
-
-// modo calendario meces: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z"/></svg>
-// modo calendario dias: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/></svg>
--- src/Componentes/Peticiones.js
+++ src/Componentes/Peticiones.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './peticiones.css';
-import logoMap from '../img/iconoMaps.png';
 
-const Peticiones = ({hora, day, month, year, setLoader }) => {
+const Peticiones = ({hora, day,setDay, month, year, setLoader,setMonth }) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [array, setArray] = useState([])
+  const [letra, setLetra] = useState('')
+
+  //const url = 'http://localhost:5000/2024'
+  const url = 'https://raw.githubusercontent.com/hernanveyret/farmaciasDeTurnoSN/main/src/Api/farmacias2024.json'
 
   const convertToTime = (timeStr) => {
     const [hours, minutes, seconds] = timeStr.split(':');
@@ -16,34 +20,12 @@ const Peticiones = ({hora, day, month, year, setLoader }) => {
   const inicio = convertToTime('00:00:00');
   const fin = convertToTime('08:30:00');
 
-  let url = `https://server-farmacias.vercel.app/api/farmacias/${year}/${month + 1}/${day}`
-  
-
   useEffect(() => {
-   
-    if(horaActual >= inicio && horaActual <= fin){
-      //console.log('muestra letra del dia anteior')
-      setLoader(true);
-      axios.get(`https://server-farmacias.vercel.app/api/farmacias/${year}/${month + 1}/${day - 1}`)
-        .then(response => {
-         // console.log(response)
-          //console.log(response.data)
-          setData(response.data);
-          setLoader(false);
-        })
-        .catch(error => {
-          console.error('Error al obtener los datos:', error);
-          setError('Hubo un error al obtener los datos.');
-          setLoader(false);
-        });
-
-    }else{
-      //console.log('muestra letra del dia actual')
-      setLoader(true);
-      axios.get(url)
+  
+    setLoader(true);
+    axios.get(url)
       .then(response => {
-       // console.log(response)
-        //console.log(response.data)
+       //console.log(response) 
         setData(response.data);
         setLoader(false);
       })
@@ -52,9 +34,47 @@ const Peticiones = ({hora, day, month, year, setLoader }) => {
         setError('Hubo un error al obtener los datos.');
         setLoader(false);
       });
+  }, []);
+
+useEffect(() => {
+
+  if(horaActual >= inicio && horaActual <= fin){
+    //console.log('muestra letra del dia anteior')
+    if (data) {
+      setLoader(true)
+      const newArray = [];
+      if(data[year][month][month+1][day-2]){
+        data[year][month][month+1][day-2].pharmacies.forEach(e => {
+          newArray.push(e);
+        });
+      }else{
+        setLoader(false);
+        return
+      }
+      setArray(newArray); 
+      setLetra(data[year][month][month+1][day-2].dateShift.toUpperCase())
+      setLoader(false)
     }
     
-  }, [day, month, year, setLoader,url,hora]);
+  }else{
+    //console.log('Muestra la letra del dia actual')
+  if (data) {
+    setLoader(true);
+    const newArray = []; 
+    if(data[year][month][month+1][day-1]){
+      data[year][month][month+1][day-1].pharmacies.forEach(e => {
+        newArray.push(e);
+      });
+    }else{
+      setLoader(false);
+      return
+    }
+    setArray(newArray); 
+    setLetra(data[year][month][month+1][day-1].dateShift.toUpperCase());
+    setLoader(false);
+  }
+}
+}, [data, month, day]);
 
   if (error) {
     return <div>{error}</div>;
@@ -63,25 +83,27 @@ const Peticiones = ({hora, day, month, year, setLoader }) => {
   if (!data) {
     return <div></div>;
   }
-
   return (
     <div className="containerFarmacias">
-      <p>Letra: <span style={{ color: "green", fontWeight: "bold" }}>{data.dateShift}</span></p>
-      {data.pharmacies.map((e, i) => (
+      <h2>Datos de Farmacias</h2>
+      <p>Letra: <span style={{ color: "green", fontWeight: "bold" }}>{letra}</span></p>
+      {array.map((e, i) => (
         <div className="items" key={i}>
           <div className="infoItems">
             <p>{e.name}</p>
             <p>{e.address}</p>
+            <p>{e.tel}</p>
           </div>
           <div className="btn-container">
-          { /* <button className="btn-map">
-              <img src={logoMap} alt="Logo maps" />
-            </button>  */ }
+          <a href={`https://www.google.com/maps/search/?api=1&query=${e.lat},${e.lon}`} rel="noopener" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#EA3323">
+              <path d="M480.06-486.67q30.27 0 51.77-21.56 21.5-21.55 21.5-51.83 0-30.27-21.56-51.77-21.55-21.5-51.83-21.5-30.27 0-51.77 21.56-21.5 21.55-21.5 51.83 0 30.27 21.56 51.77 21.55 21.5 51.83 21.5ZM480-80Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z"/>
+            </svg>
+          </a>  
           </div>
         </div>
       ))}
     </div>
   );
 };
-
 export default Peticiones;
